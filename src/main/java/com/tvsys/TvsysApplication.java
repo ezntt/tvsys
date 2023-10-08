5ce787eedb558a60a9aa8afd1d52780d267becff
Eduardo Zanetta <zanetta@Eduardos-MacBook-Pro.local>
2023-10-08 14:08:44 -0300

    first commit, only spring initializr

---
+++ src/main/java/com/tvsys/TvsysApplication.java
@@ -0,0 +1,13 @@
+package com.tvsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tvsys/TvsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
